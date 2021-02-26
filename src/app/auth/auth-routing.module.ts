bb9e1c344f70f55f2a1a1e7c8ac978dfc36fffd2
harrycoa <35603910+harrycoa@users.noreply.github.com>
2021-02-25 22:48:21 -0500

    lazy load para heroes

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,37 @@
+import { RegistroComponent } from './pages/registro/registro.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NgModule, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes =[
+  {
+    path: '',
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'registro',
+        component: RegistroComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'login'
+      }
+    ]
+  }
+
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forChild ( routes )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AuthRoutingModule { }
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,3 +1,4 @@
+import { AuthRoutingModule } from './auth-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './pages/login/login.component';
@@ -10,7 +11,8 @@ import { RegistroComponent } from './pages/registro/registro.component';
     LoginComponent,
     RegistroComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRoutingModule
   ]
 })
 export class AuthModule { }
