96cdc8177f3a740e8ee7fc63a1ea3b53efcb83a2
harrycoa <35603910+harrycoa@users.noreply.github.com>
2021-04-19 22:19:03 -0500

    feat: add enviroment

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../../environments/environment';
 import { Heroe } from './../interfaces/heroes.interface';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -9,12 +10,14 @@ export class HeroesService {
 
   constructor( private http: HttpClient ) { }
 
+  private baseUrl: string = environment.baseUrl;
+
   getHeroes(): Observable<Heroe[]>{
-    return this.http.get<Heroe[]>('http://localhost:3000/heroes');
+    return this.http.get<Heroe[]>(`${ this.baseUrl }/heroes`);
   }
 
   getHeroePorId(id: string ): Observable<Heroe> {
-    return this.http.get<Heroe>(`http://localhost:3000/heroes/${ id }`);
+    return this.http.get<Heroe>(`${ this.baseUrl }/heroes/${ id }`);
 
   }
 }
